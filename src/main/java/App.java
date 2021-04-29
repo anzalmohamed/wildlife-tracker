bf2a99c2acfd8a84697f17e04f3cde0621372f8b
anzalmohamed <anzaldidah@gmail.com>
2021-04-29 10:10:22 +0300

    created template folder and added files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,19 @@
-import java.util.List;
 import java.util.HashMap;
-import java.util.Map;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.Map;
 import static spark.Spark.*;
+
+public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+    }}
