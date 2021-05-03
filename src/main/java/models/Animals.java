6429eb92e83ec559a8301fd214c8c7631f80cdb5
anzalmohamed <anzaldidah@gmail.com>
2021-05-03 08:01:37 +0300

    added code to Animals.java

--- src/main/java/models/Animals.java
+++ src/main/java/models/Animals.java
@@ -1,5 +1,55 @@
+import org.sql2o.Connection;
 package models;
 
 public class Animals {
-    
+    private double id;
+    private String name;
+    private String age;
+    private String healthStatus;
+    private String animalCategory;
+
+    private DB db=new DB();
+
+
+    public Animal(double id,String name, String age,String animalCategory,String healthStatus){
+
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.healthStatus=healthStatus;
+        this.animalCategory=animalCategory;
+
+    }
+    public double getId(){
+        return this.id;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getAge(){
+        return this.age;
+    }
+    public String getHealthStatus(){
+        return this.healthStatus;
+    }
+    public String getAnimalCategory(){
+        return this.animalCategory;
+    }
+
+
+    public boolean keep(){
+
+        db.getCon().createQuery("INSERT INTO animals (id,name,age,healthStatus,animalCategory) VALUES(:id,:name,:age,:healthStatus,:animalCategory)")
+                .addParameter("id",id)
+                .addParameter("name",name)
+                .addParameter("age",age)
+                .addParameter("healthStatus",healthStatus)
+                .addParameter("animalCategory",animalCategory)
+
+                .executeUpdate();
+        return  true;
+    }
+
+}
+
 }
