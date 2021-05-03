e6cf5cd8ec745d7d039bbed38ab992b3dc85127f
anzalmohamed <anzaldidah@gmail.com>
2021-05-03 07:48:04 +0300

    updated code

--- src/main/java/models/sightings.java
+++ src/main/java/models/sightings.java
@@ -1,5 +1,48 @@
 package models;
+import org.sql2o.Connection;
 
 public class sightings {
-    
+    private double id;
+    private String name;
+    private String location;
+    private String date;
+
+    private DB db=new DB();
+    public Sightings (double id,String name,String location,String date) {
+        this.name=name;
+        this.location=location;
+        this.date=date;
+
+    }
+    public String getname(){
+        return this.name;
+    }
+    public double getId(){
+        return this.id;
+    }
+    public String getDate(){
+        return this.date;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public boolean save(){
+        try {
+            db.getCon().createQuery("INSERT INTO sightings(name,location,date) VALUES(:name,:location,:date)")
+                    .addParameter("name",name)
+                    .addParameter("location",location)
+                    .addParameter("date",date)
+
+                    .executeUpdate();
+            return true;
+        }
+        catch (Exception exp){
+            System.out.println(exp.getMessage());
+            return false;
+        }
+    }
+
+}
 }
