8f1ee658eb1738ee73dfef990136e7f58732ff10
anzalmohamed <anzaldidah@gmail.com>
2021-05-03 08:05:55 +0300

    commitig updates on files

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,10 +1,2 @@
-//import org.sql2o.*;
-//
-//public class DB {
-//    String connectionString = "jdbc:postgresql://localhost:5432/jadle_test"; //!
-//    Sql2o sql2o = new Sql2o(connectionString, null, null); //!
-//
-//}
-//public class DB<postgres> {
-////    postgres: //jdtybbkfkrhdrv:4dabcb2caa523a6e1ac9d708a9caf9457fffaa5576e0ddaa798aca4bf8ea052c@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d4avbrsjhnchk4
-//}
+import org.sql2o.*;
+import java.util.List;
